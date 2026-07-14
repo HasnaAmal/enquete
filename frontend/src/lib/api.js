82075519f3619fd7dff578f484cd947e6cd7dd69
meiev3lyn <hasna0amal@gmail.com>
2026-07-14 22:50:55 +0100

    Refactor API client code for better readability

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,16 +2,21 @@ import axios from 'axios';
 
 const client = axios.create({
   baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' }
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export const api = {
-  getForms:       ()                  => client.get('/forms').then(r => r.data),
-  getForm:        (id)                => client.get(`/forms/${id}`).then(r => r.data),
-  createForm:     (data)              => client.post('/forms', data).then(r => r.data),
-  updateForm:     (id, data)          => client.put(`/forms/${id}`, data).then(r => r.data),
-  deleteForm:     (id)                => client.delete(`/forms/${id}`).then(r => r.data),
-  saveQuestions:  (formId, questions) => client.post(`/forms/${formId}/questions`, { questions }).then(r => r.data),
-  submitResponse: (formId, answers)   => client.post(`/forms/${formId}/responses`, { answers }).then(r => r.data),
-  getResponses:   (formId)            => client.get(`/forms/${formId}/responses`).then(r => r.data),
+  getForms: () => client.get('/forms').then(r => r.data),
+  getForm: (id) => client.get(`/forms/${id}`).then(r => r.data),
+  createForm: (data) => client.post('/forms', data).then(r => r.data),
+  updateForm: (id, data) => client.put(`/forms/${id}`, data).then(r => r.data),
+  deleteForm: (id) => client.delete(`/forms/${id}`).then(r => r.data),
+  saveQuestions: (formId, questions) =>
+    client.post(`/forms/${formId}/questions`, { questions }).then(r => r.data),
+  submitResponse: (formId, answers) =>
+    client.post(`/forms/${formId}/responses`, { answers }).then(r => r.data),
+  getResponses: (formId) =>
+    client.get(`/forms/${formId}/responses`).then(r => r.data),
 };
