8461a83db80d16f5efdd86c3e2f965da82d12975
meiev3lyn <hasna0amal@gmail.com>
2026-07-14 19:23:47 +0100

    Update app.mjs

--- frontend/backend/app.mjs
+++ frontend/backend/app.mjs
@@ -1,11 +1,24 @@
-import express from "express";
-import routes from "./routes/index.mjs";
-import errorHandler from "./middleware/errorHandler.mjs";
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import routes from './routes/index.mjs';
+import errorHandler from './middleware/errorHandler.mjs';
 
 const app = express();
 
+app.use(
+  cors({
+    origin: 'https://tourmaline-tarsier-97df31.netlify.app'
+  })
+);
+
 app.use(express.json());
-app.use("/", routes);
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Backend is working' });
+});
+
+app.use('/', routes);
 app.use(errorHandler);
 
 export default app;
