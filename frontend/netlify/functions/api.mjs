da7e8089206dfad0ab43d2beb4e16941aa6db319
meiev3lyn <hasna0amal@gmail.com>
2026-07-14 19:35:11 +0100

    Update api.mjs

--- frontend/netlify/functions/api.mjs
+++ frontend/netlify/functions/api.mjs
@@ -1,13 +1,4 @@
-import express, { Router } from "express";
-import serverless from "serverless-http";
-
-const app = express();
-const router = Router();
-
-router.get("/hello", (req, res) => {
-  res.json({ message: "Backend is working" });
-});
-
-app.use("/api", router);
+import serverless from 'serverless-http';
+import app from '../../backend/app.mjs';
 
 export const handler = serverless(app);
