26ca0005223b5dd3c8dd493514a2209e9eeda0e6
meiev3lyn <hasna0amal@gmail.com>
2026-07-14 20:36:51 +0100

    Update app.mjs

--- frontend/backend/app.mjs
+++ frontend/backend/app.mjs
@@ -13,11 +13,12 @@ app.use(cors({
 
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.json({ message: 'Backend is working' });
 });
 
-app.use('/', routes);
+app.use('/api', routes);
+
 app.use(errorHandler);
 
 export default app;
