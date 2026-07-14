9b3e5288c5446dd909d22257f01698d3f95fb13d
meiev3lyn <hasna0amal@gmail.com>
2026-07-14 20:29:45 +0100

    Change API base URL to relative path '/api'

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+  baseURL: '/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
 export const api = {
-  getForms:       ()                   => client.get('/forms').then(r => r.data),
-  getForm:        (id)                 => client.get(`/forms/${id}`).then(r => r.data),
-  createForm:     (data)               => client.post('/forms', data).then(r => r.data),
-  updateForm:     (id, data)           => client.put(`/forms/${id}`, data).then(r => r.data),
-  deleteForm:     (id)                 => client.delete(`/forms/${id}`).then(r => r.data),
-  saveQuestions:  (formId, questions)  => client.post(`/forms/${formId}/questions`, { questions }).then(r => r.data),
-  submitResponse: (formId, answers)    => client.post(`/forms/${formId}/responses`, { answers }).then(r => r.data),
-  getResponses:   (formId)             => client.get(`/forms/${formId}/responses`).then(r => r.data),
+  getForms:       ()                  => client.get('/forms').then(r => r.data),
+  getForm:        (id)                => client.get(`/forms/${id}`).then(r => r.data),
+  createForm:     (data)              => client.post('/forms', data).then(r => r.data),
+  updateForm:     (id, data)          => client.put(`/forms/${id}`, data).then(r => r.data),
+  deleteForm:     (id)                => client.delete(`/forms/${id}`).then(r => r.data),
+  saveQuestions:  (formId, questions) => client.post(`/forms/${formId}/questions`, { questions }).then(r => r.data),
+  submitResponse: (formId, answers)   => client.post(`/forms/${formId}/responses`, { answers }).then(r => r.data),
+  getResponses:   (formId)            => client.get(`/forms/${formId}/responses`).then(r => r.data),
 };
